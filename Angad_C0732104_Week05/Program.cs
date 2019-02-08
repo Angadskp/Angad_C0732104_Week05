188fb6c3b609eb82a18c2d767b57ea91e5fd3ddb
visualstudio <visualstudio@122-PC05>
2019-02-08 18:09:12 -0500

    Commit 6 pm

--- Angad_C0732104_Week05/Program.cs
+++ Angad_C0732104_Week05/Program.cs
@@ -13,14 +13,29 @@ namespace Angad_C0732104_Week05
     class Node
     {
         public Node() { }
-        Node floor;
-        Node elevatorUP;
-        string FloorNumber;
+        public Node floor;
+        public Node elevatorUP;
+        public string FloorNumber;
 
     }
 
     class Elevator
     {
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
 
+        public void run()
+        {
+            FirstFloor = new Node();
+            FirstFloor.FloorNumber = "First Floor";
+            Console.WriteLine("Floor number is {0}", FirstFloor.FloorNumber);
+            FirstFloor.elevatorUP = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+
+            Console.WriteLine("Floor number is {0}", SecondFloor.FloorNumber);
+            
+        }
     }
 }
